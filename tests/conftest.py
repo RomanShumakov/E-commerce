3fbba82211ec6552b249e7da559fe7b46a38353b
Роман Шумаков <romeo46100@mail.ru>
2026-06-28 23:20:03 +0300

    done tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from src.classes import Product, Category
+
+
+@pytest.fixture
+def product_iphone():
+    return Product("Iphone 17 Pro Max", "512GB, Orange", 210000.0, 7)
+
+
+@pytest.fixture
+def category_smartphones():
+    return Category("Смартфоны",
+                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         ["product1", "product2", "product3"])
