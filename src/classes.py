fe089bb6b5742733f087f9c5d6b4c7bd6dc893ec
Роман Шумаков <romeo46100@mail.ru>
2026-07-07 15:24:01 +0300

    private __products attribute, comlete add_product function and getter products

--- src/classes.py
+++ src/classes.py
@@ -25,6 +25,17 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, new_product: Product):
+        self.__products.append(new_product)
+
+    @property
+    def products(self):
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n")
+        return "\n".join(result)
+
