834acd2521b058e154d25c75f4324a9db27bf995
Роман Шумаков <romeo46100@mail.ru>
2026-07-19 12:02:08 +0300

    added subclasses Smartphone and LawnGrass

--- src/classes.py
+++ src/classes.py
@@ -71,3 +71,32 @@ class Category:
             result.append(str(product))
         return "\n".join(result)
 
+
+class Smartphone(Product):
+    """ Класс продукта 'Смартфон' """
+    efficiency: str
+    model: str
+    memory:str
+    color: str
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """ Класс продукта 'трава газонная' """
+
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
