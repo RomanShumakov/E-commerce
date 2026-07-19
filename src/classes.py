4dd631e717a18387a93bdd62976e3b396c7f8c20
Роман Шумаков <romeo46100@mail.ru>
2026-07-19 15:29:58 +0300

    added condition isinstance Product in func add_product

--- src/classes.py
+++ src/classes.py
@@ -63,8 +63,11 @@ class Category:
         return f"{self.name}, количество продуктов: {total_quantity} шт."
 
     def add_product(self, new_product: Product):
-        self.__products.append(new_product)
-        Category.product_count += 1
+        if isinstance(new_product, Product):
+            self.__products.append(new_product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self):
