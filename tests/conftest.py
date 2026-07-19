36c0efdd29a35f65feb8f742fa274097920eabec
Роман Шумаков <romeo46100@mail.ru>
2026-07-19 15:55:59 +0300

    added fixtures  of Smartphone and LawnGrass in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.classes import Category, Product
+from src.classes import Category, Product, Smartphone, LawnGrass
 
 
 @pytest.fixture
@@ -15,3 +15,22 @@ def category_smartphones(product_iphone):
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product_iphone],
     )
+
+
+@pytest.fixture
+def smartphone_1():
+    return Smartphone("Iphone 15", "512GB", 210000.0, 8, "super effective", "IPhone", "512GB", "Gray space")
+
+@pytest.fixture
+def smartphone_2():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, 200MP камера", 180000.0, 5, "effective", "Samsung", "256GB", "Серый цвет")
+
+@pytest.fixture
+def lawngrass_1():
+    return LawnGrass("grass", "трава для садоводства", 500, 30, "Russia", "1 month", "green")
+
+@pytest.fixture
+def lawngrass_2():
+    return LawnGrass("grass", "трава для садоводства", 500, 30, "USA", "10 years", "sky blue")
+
+
