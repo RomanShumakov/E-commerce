f39aa72128d72e707db78bb87c2721952e509808
Роман Шумаков <romeo46100@mail.ru>
2026-07-26 20:59:00 +0300

    done linters

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.classes import BaseProduct, LawnGrass, Product, Smartphone, Category
+from src.classes import BaseProduct, Category, LawnGrass, Product, Smartphone
 
 
 def test_product_init(product_iphone):
@@ -136,21 +136,22 @@ def test_repr_format_for_different_classes():
     assert repr(smartphone).startswith("Smartphone(")
     assert "'Samsung'" in repr(smartphone)
 
+
 def test_product_zero_quantity_error():
     """Тестируем перехват ошибки ValueError при создании товара с нулевым количеством"""
     with pytest.raises(ValueError) as excinfo:
         Product("Сломанный телефон", "Описание", 50000.0, 0)
     assert str(excinfo.value) == "Товар с нулевым количеством не может быть добавлен"
 
+
 def test_category_average_price(category_smartphones):
     """Тестируем средний ценник в категории с товарами"""
-    # В category_smartphones из фикстуры уже лежит 1 товар: Iphone 17 Pro Max за 210000.0
-    # Добавим второй, чтобы проверить расчет среднего арифметического ценников
     another_phone = Product("Бюджетный телефон", "Описание", 90000.0, 5)
     category_smartphones.add_product(another_phone)
 
     # Ожидаемое среднее: (210000.0 + 90000.0) / 2 = 150000.0
     assert category_smartphones.average_price() == 150000.0
+
 
 def test_category_average_price_empty():
     """Тестируем, что для пустой категории метод average_price вернет 0"""
