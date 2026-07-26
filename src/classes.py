dfc039cf9edaba60fc86e09fc059277b5bd590dc
Роман Шумаков <romeo46100@mail.ru>
2026-07-26 20:44:03 +0300

    added ValueError for zero count of new product

--- src/classes.py
+++ src/classes.py
@@ -29,6 +29,9 @@ class Product(PrintMixin, BaseProduct):
     """Класс описания продукта"""
 
     def __init__(self, name, description, price, quantity):
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+
         self.name = name
         self.description = description
         self.__price = price
